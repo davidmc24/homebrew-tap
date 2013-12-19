5a097779326b5bd8a70aa58ade4e54830e8f61e8
David M. Carr <dcarr@commercehub.com>
2013-12-19 08:22:34 -0500

    update to lazybones 0.5

--- lazybones.rb
+++ lazybones.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Lazybones < Formula
   homepage 'https://github.com/pledbrook/lazybones'
-  url 'http://dl.bintray.com/pledbrook/lazybones-templates/lazybones-0.4.zip'
-  sha1 'fbba31014a28a0e3ed3317b55f5adf0f5eafb6fd'
+  url 'http://dl.bintray.com/pledbrook/lazybones-templates/lazybones-0.5.zip'
+  sha1 'c4ff553149e7b3dfde4ca8e6c9117d318320f879'
 
   def install
     bin.install 'bin/lazybones'
